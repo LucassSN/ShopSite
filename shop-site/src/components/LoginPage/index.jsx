63f8a77073b2f4af7e4a1e3b700069fef3d1703f
LucassSN <lucassouzaemp@gmail.com>
2025-02-13 17:18:47 -0300

    First Commit

---
+++ shop-site/src/components/LoginPage/index.jsx
@@ -0,0 +1,20 @@
+import './Login.css';
+const LoginPage = ()=>{
+  return(
+    <div className = "main">
+
+    <div>ShopSite</div>
+    <label>Usuario</label>
+    <input type="text"></input><br/><br/>
+    <label>Senha</label>
+    <input type="password"></input><br />
+    <button>Login</button>
+
+
+
+    </div>
+   
+  )
+}
+
+export default LoginPage; 
