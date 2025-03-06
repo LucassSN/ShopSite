cfb98591726247f9398a80a2359db9d0d9bf89f6
LucassSN <lucassouzaemp@gmail.com>
2025-03-06 17:51:11 -0300

    added login authentication with fixed variables

--- shop-site/src/App.jsx
+++ shop-site/src/App.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
 
-function App() {
+const App = () => {
 
   return (
-    <LoginPage/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    
+    </BrowserRouter>
     
   
   )
--- shop-site/src/components/LoginPage/index.jsx
+++ shop-site/src/components/LoginPage/index.jsx
@@ -1,22 +1,59 @@
+import React, { useState } from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
+
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate()
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+
+    const correctEmail = 'user123'
+    const correctPassword = 'senha123'
+
+    if (email == correctEmail && password == correctPassword) {
+      navigate('/');
+    } else {
+      alert('Senha ou usuario incorreto');
+    }
+
+  }
+
+
   return (
     <div className="main">
-      <form action="">
+      <form onSubmit={handleSubmit}>
 
         <h1>ShopSite</h1>
         <div className="input-box">
-          <input type="text" placeholder='Usuario'required></input>
+          <input 
+          type="text" 
+          value = {email} 
+          placeholder='Usuario' 
+          required 
+          onChange = {(e) => setEmail(e.target.value)}></input>
         </div>
         <div className="input-box">
-          <input type="password" placeholder='Senha' required></input>
+          <input type="password"  
+          value = {password} 
+          placeholder='Senha' 
+          required 
+          onChange = {(e)=> setPassword(e.target.value)}></input>
         </div>
         <button type="submit">Login</button>
       </form>
 
     </div>
 
+
+
+
+
   )
 }
 
-export default LoginPage; 
+export default LoginPage;
